53970e7a9ce6a59f7968fee2eab3be73ba06a581
Sahil Satpute <explore.sahil1@gmail.com>
2023-05-20 00:17:53 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportTrailingSlash: true,
-  experimental: {
-    output: "export",
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
